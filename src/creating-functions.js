c72fe9844818cde95fdac275a015c2f49710a1e5
ISecka <ISecka@N-NO-01-01-0093>
2024-09-23 10:32:27 +0200

    Core done

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function Increment(number)  {
+  return number + 1
+}
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +21,9 @@
 // Hello | Hello
 
 // TODO: write code below
+function Capitalise(string)  {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +35,9 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function Greet(name)  {
+  return "Hi, "+ name.charAt(0).toUpperCase() + name.slice(1)+" :)"
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +50,16 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function CheckStrings(data) {
+  numberOfStrings = 0
+  for(let i = 0 ; i < data.length ; i ++){
+    if(typeof data[i] === "string" ){
+      numberOfStrings++;
+    }
+
+  }
+  return numberOfStrings
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +71,18 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function AddEd(object){
+  if (typeof object === "object")
+    if(!object.hasOwnProperty("edward"))
+    object.edward = "amazing"
+  return object
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: Increment, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: Capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: Greet, // etc
+  d: CheckStrings,
+  e: AddEd
 }
